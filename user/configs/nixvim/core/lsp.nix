9d693bd897d002f10b61d79b7833161fa6ed300f
Gideon Wolfe <wolfegideon@gmail.com>
2024-07-02 11:37:32 -0400

    added lsp_signature

--- user/configs/nixvim/core/lsp.nix
+++ user/configs/nixvim/core/lsp.nix
@@ -1,130 +1,100 @@
 {
-    # Main Telescope plugin
-    programs.nixvim.plugins.lsp= {
+  # Main Telescope plugin
+  programs.nixvim.plugins.lsp = {
 
-        # Enable LSPs to be configured
+    # Enable LSPs to be configured
+    enable = true;
+
+    # onAttach = ''
+    #   require "lsp_signature".on_attach(client, bufnr)
+    # '';
+    onAttach = ''
+            require "lsp_signature".on_attach({
+                bind = true,
+                handler_opts = {
+                  border = "rounded"
+                },
+                hint_enable = true,
+      		  hint_prefix = " ",
+            }, bufnr)
+    '';
+
+    # Configs for all LSP servers
+    servers = {
+
+      # Nix 
+      nil-ls = { enable = true; };
+
+      # Bash
+      bashls = { enable = true; };
+
+      # Clojure
+      clojure-lsp = { enable = true; };
+
+      # Cmake
+      cmake = { enable = true; };
+
+      # C#
+      csharp-ls = { enable = true; };
+
+      # CSS
+      cssls = { enable = true; };
+
+      # JS
+      eslint = { enable = true; };
+
+      # Godot Script
+      gdscript = { enable = true; };
+
+      # Go
+      gopls = { enable = true; };
+
+      # Haskell
+      hls = { enable = true; };
+
+      # html
+      html = { enable = true; };
+
+      # JSON
+      jsonls = { enable = true; };
+
+      # Julia
+      julials = { enable = true; };
+
+      # Kotlin
+      kotlin-language-server = { enable = true; };
+
+      # Lua
+      lua-ls = { enable = true; };
+
+      # PHP
+      phpactor = { enable = true; };
+
+      # Python
+      pyright = { enable = true; };
+
+      # Rust
+      rust-analyzer = {
         enable = true;
+        # automatically install deps if not already
+        installCargo = true;
+        installRustc = true;
+      };
 
-        # Configs for all LSP servers
-        servers = {
-            # Nix 
-            nil-ls = {
-                # enable nil LSP for Nix
-                enable = true;
-            };
+      # Tailwind CSS
+      tailwindcss = { enable = true; };
 
-            # Bash
-            bashls = {
-                enable = true;
-            };
+      # LaTeX
+      texlab = { enable = true; };
 
-            # Clojure
-            clojure-lsp = {
-                enable = true;
-            };
+      # Typescript
+      tsserver = { enable = true; };
 
-            # Cmake
-            cmake = {
-                enable = true;
-            };
+      # Vue JS
+      vuels = { enable = true; };
 
-            # C#
-            csharp-ls = {
-                enable = true;
-            };
-
-            # CSS
-            cssls = {
-                enable = true;
-            };
-
-            # JS
-            eslint = {
-                enable = true;
-            };
-
-            # Godot Script
-            gdscript = {
-                enable = true;
-            };
-
-            # Go
-            gopls = {
-                enable = true;
-            };
-
-            # Haskell
-            hls = {
-                enable = true;
-            };
-
-            # html
-            html = {
-                enable = true;
-            };
-
-            # JSON
-            jsonls = {
-                enable = true;
-            };
-
-            # Julia
-            julials = {
-                enable = true;
-            };
-
-            # Kotlin
-            kotlin-language-server = {
-                enable = true;
-            };
-
-            # Lua
-            lua-ls = {
-                enable = true;
-            };
-
-            # PHP
-            phpactor = {
-                enable = true;
-            };
-
-            # Python
-            pyright = {
-                enable = true;
-            };
-
-            # Rust
-            rust-analyzer = {
-                enable = true;
-                # automatically install deps if not already
-                installCargo = true;
-                installRustc = true;
-            };
-
-            # Tailwind CSS
-            tailwindcss = {
-                enable = true;
-            };
-
-            # LaTeX
-            texlab = {
-                enable = true;
-            };
-
-            # Typescript
-            tsserver = {
-                enable = true;
-            };
-
-            # Vue JS
-            vuels = {
-                enable = true;
-            };
-
-            # YAML
-            yamlls = {
-                enable = true;
-            };
-        };
+      # YAML
+      yamlls = { enable = true; };
     };
+  };
 }
--- user/configs/nixvim/nixvim.nix
+++ user/configs/nixvim/nixvim.nix
@@ -4,103 +4,73 @@ with config.lib.stylix.colors.withHashtag;
 
 {
 
-    imports = [
-        # Core
-        ./core/opts.nix
-        ./core/lsp.nix
-        ./core/highlight.nix
-        ./core/keymaps.nix
-        ./core/autocommands.nix
+  imports = [
+    # Core
+    ./core/opts.nix
+    ./core/lsp.nix
+    ./core/highlight.nix
+    ./core/keymaps.nix
+    ./core/autocommands.nix
 
-        # Plugins
+    # Plugins
 
+    # UI Plugins
+    ./plugins/ui/bufferline.nix
+    ./plugins/ui/dashboard.nix
+    ./plugins/ui/fidget.nix
+    ./plugins/ui/indent-blankline.nix
+    ./plugins/ui/lspkind.nix
+    ./plugins/ui/lualine.nix
+    ./plugins/ui/noice.nix
+    ./plugins/ui/nvim-colorizer.nix
+    ./plugins/ui/nvim-tree.nix
+    ./plugins/ui/nvim-ufo.nix
+    ./plugins/ui/rainbow-delimiters.nix
+    ./plugins/ui/telescope.nix
+    ./plugins/ui/trouble.nix
+    ./plugins/ui/which-key.nix
 
-        # UI Plugins
-        ./plugins/ui/bufferline.nix
-        ./plugins/ui/dashboard.nix
-        ./plugins/ui/fidget.nix
-        ./plugins/ui/indent-blankline.nix
-        ./plugins/ui/lspkind.nix
-        ./plugins/ui/lualine.nix
-        ./plugins/ui/noice.nix
-        ./plugins/ui/nvim-tree.nix
-        ./plugins/ui/nvim-ufo.nix
-        ./plugins/ui/rainbow-delimiters.nix
-        ./plugins/ui/telescope.nix
-        ./plugins/ui/trouble.nix
-        ./plugins/ui/which-key.nix
+    # Editing plugins
+    ./plugins/editing/nvim-autopairs.nix
+    ./plugins/editing/better-escape.nix
+    ./plugins/editing/surround.nix
+    ./plugins/editing/comment.nix
+    ./plugins/editing/cmp.nix
+    ./plugins/editing/lsp-format.nix
 
-        # Editing plugins
-        ./plugins/editing/nvim-autopairs.nix
-        ./plugins/editing/better-escape.nix
-        ./plugins/editing/surround.nix
-        ./plugins/editing/comment.nix
-        ./plugins/editing/cmp.nix
-        ./plugins/editing/lsp-format.nix
+    # Version Control Plugins
+    ./plugins/git/gitsigns.nix
+    ./plugins/git/fugitive.nix
+    ./plugins/git/diffview.nix
 
-        # Version Control Plugins
-        ./plugins/git/gitsigns.nix
-        ./plugins/git/fugitive.nix
-        ./plugins/git/diffview.nix
+    # Utils
+    ./plugins/utils/dap.nix
+    ./plugins/utils/friendly-snippets.nix
+    ./plugins/utils/hop.nix
+    ./plugins/utils/lspsaga.nix
+    ./plugins/utils/luasnip.nix
+    ./plugins/utils/markdown-preview.nix
+    ./plugins/utils/none-ls.nix
+    ./plugins/utils/project-nvim.nix
+    ./plugins/utils/treesitter.nix
+    ./plugins/utils/todo-comments.nix
+    ./plugins/utils/vimtex.nix
 
-        # Utils
-        ./plugins/utils/dap.nix
-        ./plugins/utils/friendly-snippets.nix
-        ./plugins/utils/hop.nix
-        ./plugins/utils/lspsaga.nix
-        ./plugins/utils/luasnip.nix
-        ./plugins/utils/markdown-preview.nix
-        ./plugins/utils/none-ls.nix
-        ./plugins/utils/project-nvim.nix
-        ./plugins/utils/treesitter.nix
-        ./plugins/utils/todo-comments.nix
-        ./plugins/utils/vimtex.nix
+  ];
+  programs.nixvim = {
 
-    ];
-	programs.nixvim = {
+    # Enable NixVim configuration
+    enable = true;
 
-		# Enable NixVim configuration
-		enable = true;
+    enableMan = true;
 
-        enableMan = true;
+    globals = { mapleader = " "; };
 
-        globals = {
-            mapleader = " ";
-        };
+    clipboard.providers = { wl-copy = { enable = true; }; };
+    clipboard.register = "unnamedplus";
 
-        clipboard.providers = {
-            wl-copy = {
-                enable = true;
-            };
-        };
-        clipboard.register = "unnamedplus";
+    #HACK: adding plugins that don't have a module yet
+    extraPlugins = with pkgs.vimPlugins; [ lazygit-nvim lsp_signature-nvim ];
 
-        # autoCmd = [
-        #     # resumes last place in file
-        #     {
-        #         event = "BufReadPost";
-        #         callback = {
-        #             __raw = ''
-        #                 function()
-        #                     vim.cmd([[
-        #                         if line("'\"") > 0 && line("'\"") <= line("$") |
-        #                             exe "normal! g`\"" |
-        #                         endif
-        #                     ]])
-        #                 end
-        #             '';
-        #         };
-        #
-        #     }
-        # ];
-
-
-
-
-        # adding plugins that don't have a module yet
-        extraPlugins = with pkgs.vimPlugins; [
-            lazygit-nvim
-        ];
-
-	};
+  };
 }
