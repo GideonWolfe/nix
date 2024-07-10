9835dc0d194cd01ec33368ab9da1a6b1edaff4a5
Gideon Wolfe <wolfegideon@gmail.com>
2024-07-10 11:53:47 -0400

    refactoring user settings

--- configs/users/gideon/home.nix
+++ configs/users/gideon/home.nix
@@ -1,6 +1,4 @@
-{ lib, config, pkgs, ... }:
-
-with config.lib.stylix.colors.withHashtag;
+{ lib, config, ... }:
 
 {
 
@@ -59,12 +57,18 @@ with config.lib.stylix.colors.withHashtag;
     # CONFIGS #
     ###########
 
+    # Session/env variables
+    ./configs/session-variables.nix
+
     # Hyprland
     # Enable when ready for it
     ./configs/hyprland.nix
     ./configs/sway.nix
     ./configs/swaylock.nix
     ./configs/swayidle.nix
+
+    # Cursor settings
+    ./configs/cursor.nix
 
     # Neovim
     ./configs/nixvim/nixvim.nix
@@ -154,6 +158,10 @@ with config.lib.stylix.colors.withHashtag;
     # Custom startpage
     ./configs/startpage/style.nix
     ./configs/startpage/service.nix
+
+    # These configs have to be generated manually
+    ./configs/darkreader.nix
+
   ];
 
   # enable unfree package use with home manager (ie stylix referencing symbola font which is unfree)
@@ -171,92 +179,6 @@ with config.lib.stylix.colors.withHashtag;
       "  mkdir -p ${config.home.homeDirectory}/videos/screen_recordings/\n";
   };
 
-  # Specify cursor theme
-  home.pointerCursor = lib.mkForce {
-    # name = "phinger-cursors-light";
-    # package = pkgs.phinger-cursors;
-    #HACK: the capitilization may or may not be needed in the future: https://github.com/NixOS/nixpkgs/issues/321781
-    # name = "catppuccin-mocha-dark-cursors";
-    name = "Catppuccin-Mocha-Dark-Cursors"; #this creates a working symlink
-    # name = "Catppuccin-Mocha-Dark"; #this creates a working symlink
-    package = pkgs.catppuccin-cursors.mochaDark;
-    size = 32;
-    gtk.enable = true;
-  };
-
-  xdg = {
-    configFile = {
-
-      # Creates a config for DarkReader
-      # TODO, need to make this auto update?
-      darkreader = {
-        enable = true;
-        #onChange = manually tell darkreader to refresh somehow?
-        target = "darkreader/config.json";
-        text = ''
-
-          {
-              "schemeVersion": 2,
-              "enabled": true,
-              "fetchNews": true,
-              "theme": {
-          	"mode": 1,
-          	"brightness": 100,
-          	"contrast": 100,
-          	"grayscale": 0,
-          	"sepia": 0,
-          	"useFont": false,
-          	"fontFamily": "Open Sans",
-          	"textStroke": 0,
-          	"engine": "dynamicTheme",
-          	"stylesheet": "",
-          	"darkSchemeBackgroundColor": "${base00}",
-          	"darkSchemeTextColor": "${base05}",
-          	"lightSchemeBackgroundColor": "${base05}",
-          	"lightSchemeTextColor": "${base00}",
-          	"scrollbarColor": "auto",
-          	"selectionColor": "auto",
-          	"styleSystemControls": false,
-          	"lightColorScheme": "Default",
-          	"darkColorScheme": "Default",
-          	"immediateModify": false
-              },
-              "presets": [],
-              "customThemes": [],
-              "enabledByDefault": true,
-              "enabledFor": [],
-              "disabledFor": [],
-              "changeBrowserTheme": false,
-              "syncSettings": false,
-              "syncSitesFixes": true,
-              "automation": {
-          	"enabled": false,
-          	"mode": "",
-          	"behavior": "OnOff"
-              },
-              "time": {
-          	"activation": "18:00",
-          	"deactivation": "9:00"
-              },
-              "location": {
-          	"latitude": null,
-          	"longitude": null
-              },
-              "previewNewDesign": true,
-              "enableForPDF": true,
-              "enableForProtectedPages": true,
-              "enableContextMenus": false,
-              "detectDarkTheme": false,
-              "displayedNews": [
-          	"thanks-2023"
-              ]
-          }
-
-        '';
-      };
-    };
-  };
-
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
   # introduces backwards incompatible changes.
@@ -265,63 +187,6 @@ with config.lib.stylix.colors.withHashtag;
   # want to update the value, then make sure to first check the Home Manager
   # release notes.
   home.stateVersion = "23.11"; # Please read the comment before changing.
-
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
-  #home.packages = [
-  # # Adds the 'hello' command to your environment. It prints a friendly
-  # # "Hello, world!" when run.
-  # pkgs.hello
-
-  # # It is sometimes useful to fine-tune packages, for example, by applying
-  # # overrides. You can do that directly here, just don't forget the
-  # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-  # # fonts?
-  # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-  # # You can also create simple shell scripts directly inside your
-  # # configuration. For example, this adds a command 'my-hello' to your
-  # # environment:
-  # (pkgs.writeShellScriptBin "my-hello" ''
-  #   echo "Hello, ${config.home.username}!"
-  # '')
-  #];
-
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
-  home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
-  };
-
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. If you don't want to manage your shell through Home
-  # Manager then you have to manually source 'hm-session-vars.sh' located at
-  # either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/gideon/etc/profile.d/hm-session-vars.sh
-  #
-  home.sessionVariables = {
-    EDITOR = "nvim";
-    TERM = "kitty";
-    BROWSER = "firefox";
-    PAGER = "ov";
-    RULES =
-      "${config.home.homeDirectory}/nix/configs/users/gideon/secrets/secrets.nix";
-    XCURSOR_THEME = "Catppuccin-Mocha-Dark-Cursors";
-  };
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
