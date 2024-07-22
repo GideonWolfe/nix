3e07858d40d801942fdbf333fb1ced844efb0c09
Gideon Wolfe <wolfegideon@gmail.com>
2024-07-21 20:36:05 -0400

    set up zeroday repo

--- configs/users/gideon/configs/projects/zeroday.nix
+++ configs/users/gideon/configs/projects/zeroday.nix
@@ -8,7 +8,13 @@ with config.lib.stylix.colors.withHashtag;
     createZeroDay = lib.hm.dag.entryAfter [ "writeBoundary" ]
       "  mkdir -p ${config.home.homeDirectory}/projects/zeroday/zeroday/\n";
     cloneZeroDay = lib.hm.dag.entryAfter [ "writeBoundary" ]
-      "${pkgs.git}/bin/git -C ${config.home.homeDirectory}/projects/zeroday/zeroday/ pull || git clone github:gideonwolfe/zeroday ${config.home.homeDirectory}/projects/zeroday/zeroday/";
+      "GIT_SSH=${lib.getExe pkgs.openssh} ${
+        lib.getExe pkgs.git
+      } -C ${config.home.homeDirectory}/projects/zeroday/zeroday/ pull || GIT_SSH=${
+        lib.getExe pkgs.openssh
+      } ${
+        lib.getExe pkgs.git
+      } clone github:gideonwolfe/zeroday ${config.home.homeDirectory}/projects/zeroday/zeroday/";
   };
 
   # home.file.zeroDayRepo = {
