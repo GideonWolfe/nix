6da37f152a80a762013cfab977def0642820f15b
Gideon Wolfe <wolfegideon@gmail.com>
2024-07-21 20:37:34 -0400

    cleanup

--- configs/users/gideon/configs/projects/zeroday.nix
+++ configs/users/gideon/configs/projects/zeroday.nix
@@ -3,10 +3,11 @@
 with config.lib.stylix.colors.withHashtag;
 
 {
-  # Create initial project directory
   home.activation = {
+    # Create initial project directory
     createZeroDay = lib.hm.dag.entryAfter [ "writeBoundary" ]
       "  mkdir -p ${config.home.homeDirectory}/projects/zeroday/zeroday/\n";
+    # If repo is missing, clone it, if not, pull it.
     cloneZeroDay = lib.hm.dag.entryAfter [ "writeBoundary" ]
       "GIT_SSH=${lib.getExe pkgs.openssh} ${
         lib.getExe pkgs.git
@@ -16,20 +17,4 @@ with config.lib.stylix.colors.withHashtag;
         lib.getExe pkgs.git
       } clone github:gideonwolfe/zeroday ${config.home.homeDirectory}/projects/zeroday/zeroday/";
   };
-
-  # home.file.zeroDayRepo = {
-  #   enable = true;
-  #   recursive = true;
-  #   target = "projects/zeroday/zeroday";
-  #   source = builtins.fetchGit {
-  #     url = "github:gideonwolfe/zeroday";
-  #     ref = "main";
-  #   };
-  # };
-  # # zeroDayRepo = builtins.fetchFromGitHub {
-  #   url = "github:gideonwolfe/zeroday";
-  #   owner = "GideonWolfe";
-  #   repo = "zeroday";
-  #   githubBase = "github@github.com";
-  # };
 }
