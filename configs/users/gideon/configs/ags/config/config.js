9ecb029168d311fd9d7ef4d484801205ff0c77bd
Gideon Wolfe <wolfegideon@gmail.com>
2024-08-29 20:37:42 -0400

    updated startpage service

--- configs/users/gideon/configs/ags/config/config.js
+++ configs/users/gideon/configs/ags/config/config.js
@@ -1,5 +1,23 @@
+// function that outputs bars
+function Bar(monitor = 0) {
+    const myLabel = Widget.Label({
+        label: 'some example content',
+    })
+
+    Utils.interval(1000, () => {
+        myLabel.label = Utils.exec('date')
+    })
+
+    return Widget.Window({
+        monitor,
+        name: `bar${monitor}`, // this name has to be unique
+        anchor: ['top', 'left', 'right'],
+        child: myLabel,
+    })
+}
+
 App.config({
     windows: [
-        // this is where window definitions will go
+        Bar(0),
     ]
 })
