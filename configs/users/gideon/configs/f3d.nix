cccc30709ffa9dee0f83bcc0a0a2209e6f28ad3c
Gideon Wolfe <wolfegideon@gmail.com>
2025-01-15 02:20:17 -0500

    fixed bug with rgb output

--- configs/users/gideon/configs/f3d.nix
+++ configs/users/gideon/configs/f3d.nix
@@ -10,20 +10,22 @@ with config.lib.stylix.colors;
     target = "f3d/config.json";
     #HACK: docs say it's "background-color" but this is the only thing that works
     #BUG: bg-color and color don't look like my theme colors
+
+        # put this in text below to add background image
+        #"hdri-file": "${config.stylix.image}",
     text = ''
       {
           ".*": {
-              "bg-color": "${base00-dec-r},${base00-dec-b},${base00-dec-b},",
-              "color": "${base0E-dec-r},${base0E-dec-b},${base0E-dec-b},",
-              "grid-color": "${base0A-dec-r},${base0A-dec-b},${base0A-dec-b},",
+              "bg-color": "${base00-dec-r},${base00-dec-g},${base00-dec-b},",
+              "color": "${base0E-dec-r},${base0E-dec-g},${base0E-dec-b},",
+              "grid-color": "${base0A-dec-r},${base0A-dec-g},${base0A-dec-b},",
               "ambient-occlusion": true,
               "grid": true,
               "grid-absolute": true,
               "up": "+Z",
               "watch": true,
-              "hdri-skybox": true,
-              "hdri-file": "${config.stylix.image}",
-              "blur-background": true
+              "hdri-skybox": false,
+              "blur-background": false
           }
       }
     '';
