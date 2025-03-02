f28bcb89a7abae91763126a131edc70eff364757
Gideon Wolfe <wolfegideon@gmail.com>
2025-03-02 15:05:47 -0500

    change cava colors

--- configs/users/gideon/configs/cava.nix
+++ configs/users/gideon/configs/cava.nix
@@ -16,10 +16,10 @@ with config.lib.stylix.colors.withHashtag;
 			color = {
 				gradient = 1;
 				gradient_count = 4;
-				gradient_color_1 = "'${base06}'";
-				gradient_color_2 = "'${base09}'";
+				gradient_color_1 = "'${base0E}'";
+				gradient_color_2 = "'${base0B}'";
 				gradient_color_3 = "'${base0A}'";
-				gradient_color_4 = "'${base0E}'";
+				gradient_color_4 = "'${base06}'";
 			};
 
 			output = {
