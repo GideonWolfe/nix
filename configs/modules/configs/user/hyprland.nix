14571c52824a898977dd636228be7ee30f1b00a3
Gideon Wolfe <wolfegideon@gmail.com>
2025-03-20 02:49:36 -0400

    refactored into nix

--- configs/modules/configs/user/hyprland.nix
+++ configs/modules/configs/user/hyprland.nix
@@ -13,14 +13,163 @@ with config.lib.stylix.colors;
     systemd.enable = true;
 
     settings = {
+
+      # Set Windows key as mod key
       "$mod" = "SUPER";
+
+      # reference var for launching our menu
       "$menu" = "pkill wofi || wofi --show run";
 
-      #bindm = [
-      #"$mod, Return, exec, kitty"
-      #"$mod Shift, F, fullscreen"
-      #"$mod Shift, Q, killactive"
-      #];
+      # Commands that will get executed on login
+      exec-once = [
+        # Start HyprPanel
+        "hyprpanel"
+
+        # Or start waybar
+        #"pkill waybar & sleep 0.5 && waybar"
+
+        # Start Kando
+        "kando"
+
+        # Set the correct cursor
+        "hyprctl setcursor ${config.home.pointerCursor.name} ${
+          builtins.toString (config.home.pointerCursor.size + 8)
+        }"
+      ];
+
+      # Mouse bindings
+      bindm = [
+        # mod+left click to move window
+        "$mod, mouse:272, movewindow"
+        # mod+right click to resize window
+        "$mod, mouse:273, resizewindow"
+      ];
+
+      # e means it can get "held"
+      # l means it works on the lockscreen
+      bindel = [
+        # Raise volume key or volume wheel
+        ", XF86AudioRaiseVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 2%+"
+        # Lower volume key or volume wheel
+        ", XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 2%-"
+        # Brightness buttons
+        ",XF86MonBrightnessDown, exec, sudo /run/current-system/sw/bin/light -U 10"
+        ",XF86MonBrightnessUp, exec, sudo /run/current-system/sw/bin/light -A 10"
+      ];
+
+      # l means it works on the lockscreen
+      bindl = [
+        # Mute key toggle
+        ", XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
+        # Mute Mic key toggle
+        ", XF86AudioMicMute, exec, wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"
+        # Play/Pause button
+        ", XF86AudioPlay, exec, playerctl play-pause"
+        # Previous button
+        ", XF86AudioPrev, exec, playerctl previous"
+        # Next button
+        ", XF86AudioNext, exec, playerctl next"
+
+        # Hyprlock when the lid is closed
+        ",switch:on:Lid Switch, exec, hyprctl dispatch exec hyprlock && systemctl suspend"
+      ];
+
+      # Key bindings
+      bind = [
+
+        # Open terminal
+        "$mod, RETURN, exec, kitty"
+        # Close active window
+        "$mod SHIFT, Q, killactive"
+        # Toggle fullscreen on active window
+        "$mod SHIFT, F, fullscreen, 2"
+        # Toggle floating on active window
+        "$mod, V, togglefloating,"
+
+        # Toggle group for the window
+        "$mod SHIFT, G, togglegroup,"
+        # Cycle windows within a group
+        "$mod, G, changegroupactive,"
+
+        # Move focus with arrow keys
+        "$mod, left, movefocus, l"
+        "$mod, right, movefocus, r"
+        "$mod, up, movefocus, u"
+        "$mod, down, movefocus, d"
+        # Move focus with vim keybinds
+        "$mod, h, movefocus, l"
+        "$mod, l, movefocus, r"
+        "$mod, k, movefocus, u"
+        "$mod, j, movefocus, d"
+
+        # mod+shift+direction moves windows
+        "$mod SHIFT, Left, movewindoworgroup, l"
+        "$mod SHIFT, Right, movewindoworgroup, r"
+        "$mod SHIFT, Up, movewindoworgroup, u"
+        "$mod SHIFT, Down, movewindoworgroup, d"
+        "$mod SHIFT, h, movewindoworgroup, l"
+        "$mod SHIFT, l, movewindoworgroup, r"
+        "$mod SHIFT, k, movewindoworgroup, u"
+        "$mod SHIFT, j, movewindoworgroup, d"
+
+        # Switch workspaces with mod+<number>
+        "$mod, 1, workspace, 1"
+        "$mod, 2, workspace, 2"
+        "$mod, 3, workspace, 3"
+        "$mod, 4, workspace, 4"
+        "$mod, 5, workspace, 5"
+        "$mod, 6, workspace, 6"
+        "$mod, 7, workspace, 7"
+        "$mod, 8, workspace, 8"
+        "$mod, 9, workspace, 9"
+        "$mod, 0, workspace, 10"
+
+        # Move active window to a workspace with mod + SHIFT + [0-9]
+        "$mod SHIFT, 1, movetoworkspace, 1"
+        "$mod SHIFT, 2, movetoworkspace, 2"
+        "$mod SHIFT, 3, movetoworkspace, 3"
+        "$mod SHIFT, 4, movetoworkspace, 4"
+        "$mod SHIFT, 5, movetoworkspace, 5"
+        "$mod SHIFT, 6, movetoworkspace, 6"
+        "$mod SHIFT, 7, movetoworkspace, 7"
+        "$mod SHIFT, 8, movetoworkspace, 8"
+        "$mod SHIFT, 9, movetoworkspace, 9"
+        "$mod SHIFT, 0, movetoworkspace, 10"
+
+        # Menus
+        # Kando desktop menu
+        "CTRL, Space, global, kando:desktop-menu"
+        # Execute wofi menu
+        "$mod, D, exec, $menu"
+        # Execute hyprland window switching script
+        "$mod SHIFT, P, exec, ${pkgs.bash}/bin/bash ${config.home.homeDirectory}/nix/configs/modules/scripts/user/desktop/navigation/hyprland-window-switcher.sh"
+        # Execute wofi web search script
+        "$mod SHIFT, S, exec, ${pkgs.bash}/bin/bash ${config.home.homeDirectory}/nix/configs/modules/scripts/user/desktop/search/wofi-web-search.sh"
+        # Wofi-emoji
+        "$mod SHIFT, E, exec, ${lib.getExe pkgs.wofi-emoji}"
+
+        # Screenshots - piped to swappy for editing after capture
+        # Take screenshot of output with printscreen key
+        ", PRINT, exec, hyprshot -m output --raw | swappy -f -"
+        # Take screenshot of window with mod+printscreen key
+        "$mod, PRINT, exec, hyprshot -m window --raw | swappy -f -"
+        # Take screenshot of region with mod+shift+printscreen key
+        "$mod SHIFT, PRINT, exec, hyprshot -m region --raw | swappy -f -"
+
+        # Toggle calculator scratchpad
+        "$mod, M, exec, hyprctl clients | grep 'kitty-calculator' && hyprctl dispatch togglespecialworkspace calculator || kitty --class 'kitty-calculator' qalc &"
+
+        # Toggle translator scratchpad
+        "$mod, T, exec, hyprctl clients | grep 'kitty-translator' && hyprctl dispatch togglespecialworkspace translator || kitty --class 'kitty-translator' trans -theme random -I &"
+
+        # Toggle AI scratchpad
+        "$mod, A, exec, hyprctl clients | grep 'kitty-ai' && hyprctl dispatch togglespecialworkspace ai || kitty --class 'kitty-ai' tenere &"
+        #"$mod, A, exec, hyprctl clients | grep 'kitty-ai' && hyprctl dispatch togglespecialworkspace ai || kitty --class 'kitty-ai' tgpt -m &"
+
+        # toggle for workspace overview
+        "SUPER, grave, hyprexpo:expo, toggle" # can be: toggle, off/disable or on/enable
+        #"SUPER, grave, overview:toggle"
+      ];
 
       group = {
         "col.border_active" = lib.mkForce "rgb(${base0D})";
@@ -64,101 +213,51 @@ with config.lib.stylix.colors;
         };
       };
 
+      # Window rules for scratchpads and other programs
+      windowrulev2 = [
+        # Calculator Scratchpad
+        "float,class:(kitty-calculator)"
+        "size 500 500,class:(kitty-calculator)"
+        "workspace special:calculator,class:(kitty-calculator)"
+
+        # Translator Scratchpad
+        "float,class:(kitty-translator)"
+        "size 800 800,class:(kitty-translator)"
+        "workspace special:translator,class:(kitty-translator)"
+
+        # AI Scratchpad
+        "float,class:(kitty-ai)"
+        "size 800 800,class:(kitty-ai)"
+        "workspace special:ai,class:(kitty-ai)"
+
+        # To make KDE connect message reply window not be tiny
+        "minsize 400 400, title:(Messages — KDE Connect Daemon)"
+      ];
+
+      # legacy windowrule to make Kando work
+      # TODO look into updating to windowrulev2
+      windowrule = [
+        "noblur, kando"
+        "opaque, kando"
+        "size 100% 100%, kando"
+        "noborder, kando"
+        "noanim, kando"
+        "float, kando"
+        "pin, kando"
+      ];
+
+      # Needed to fix scaling bug
+      xwayland = { force_zero_scaling = true; };
+
     };
     extraConfig = ''
-      #exec = pkill waybar & sleep 0.5 && waybar
-
-      exec-once = hyprpanel
-      exec-once = kando
-
-      #exec-once = hyprctl setcursor catppuccin-mocha-dark-cursors 40
-      exec-once = hyprctl setcursor ${config.home.pointerCursor.name} ${
-        builtins.toString (config.home.pointerCursor.size + 8)
-      }
-      #env = HYPRCURSOR_THEME,catppuccin-mocha-dark-cursors
-      #env = HYPRCURSOR_SIZE,40
-
+      # generalized monitor rule for hotplugging displays
+      # set here so per-host configs don't conflict
       monitor=,preferred,auto,1
 
-      # fixes scaling issue (stuff was too big)
-      xwayland {
-          force_zero_scaling = true
-      }
 
-      bind = CTRL, Space, global, kando:desktop-menu
-
-      bind = $mod, RETURN, exec, kitty
-      bind = $mod SHIFT, Q, killactive
-      #bind = $mod SHIFT, F, fullscreen
-      bind = $mod SHIFT, F, fullscreen, 2
-      #bind = $mod, M, exit,
-      bind = $mod SHIFT, P, exec, ${pkgs.bash}/bin/bash ${config.home.homeDirectory}/nix/configs/users/gideon/scripts/system/navigation/hyprland-window-switcher.sh
-      bind = $mod SHIFT, S, exec, ${pkgs.bash}/bin/bash ${config.home.homeDirectory}/nix/configs/users/gideon/scripts/system/search/wofi-web-search.sh
-      bind = $mod SHIFT, E, exec, ${lib.getExe pkgs.wofi-emoji}
-      bind = $mod SHIFT, G, togglegroup,
-      bind = $mod, G, changegroupactive,
-      bind = $mod, V, togglefloating,
-      bind = $mod, D, exec, $menu
-      bind = $mod, P, pseudo, # dwindle
-      bind = $mod, J, togglesplit, # dwindle
-
-      # Move/resize windows with mainMod + LMB/RMB and dragging
-      bindm = $mod, mouse:272, movewindow
-      bindm = $mod, mouse:273, resizewindow
-
-      # Move focus with mod + arrow keys
-      bind = $mod, left, movefocus, l
-      bind = $mod, right, movefocus, r
-      bind = $mod, up, movefocus, u
-      bind = $mod, down, movefocus, d
-      # vim bindings
-      bind = $mod, h, movefocus, l
-      bind = $mod, l, movefocus, r
-      bind = $mod, k, movefocus, u
-      bind = $mod, j, movefocus, d
-
-      #bind = $mod SHIFT, Left, movewindow, l
-      #bind = $mod SHIFT, Right, movewindow, r
-      #bind = $mod SHIFT, Up, movewindow, u
-      #bind = $mod SHIFT, Down, movewindow, d
-      #bind = $mod SHIFT, h, movewindow, l
-      #bind = $mod SHIFT, l, movewindow, r
-      #bind = $mod SHIFT, k, movewindow, u
-      #bind = $mod SHIFT, j, movewindow, d
-
-      bind = $mod SHIFT, Left, movewindoworgroup, l
-      bind = $mod SHIFT, Right, movewindoworgroup, r
-      bind = $mod SHIFT, Up, movewindoworgroup, u
-      bind = $mod SHIFT, Down, movewindoworgroup, d
-      bind = $mod SHIFT, h, movewindoworgroup, l
-      bind = $mod SHIFT, l, movewindoworgroup, r
-      bind = $mod SHIFT, k, movewindoworgroup, u
-      bind = $mod SHIFT, j, movewindoworgroup, d
-
-      # Switch workspaces with mod + [0-9]
-      bind = $mod, 1, workspace, 1
-      bind = $mod, 2, workspace, 2
-      bind = $mod, 3, workspace, 3
-      bind = $mod, 4, workspace, 4
-      bind = $mod, 5, workspace, 5
-      bind = $mod, 6, workspace, 6
-      bind = $mod, 7, workspace, 7
-      bind = $mod, 8, workspace, 8
-      bind = $mod, 9, workspace, 9
-      bind = $mod, 0, workspace, 10
-
-      # Move active window to a workspace with mod + SHIFT + [0-9]
-      bind = $mod SHIFT, 1, movetoworkspace, 1
-      bind = $mod SHIFT, 2, movetoworkspace, 2
-      bind = $mod SHIFT, 3, movetoworkspace, 3
-      bind = $mod SHIFT, 4, movetoworkspace, 4
-      bind = $mod SHIFT, 5, movetoworkspace, 5
-      bind = $mod SHIFT, 6, movetoworkspace, 6
-      bind = $mod SHIFT, 7, movetoworkspace, 7
-      bind = $mod SHIFT, 8, movetoworkspace, 8
-      bind = $mod SHIFT, 9, movetoworkspace, 9
-      bind = $mod SHIFT, 0, movetoworkspace, 10
-
+      # Due to limitations of nix syntax, 
+      # this has to be configured thru extraConfig
       # i3 like resize submap
       # uses binde to allow holding
       bind = $mod, R,submap,resize
@@ -173,74 +272,6 @@ with config.lib.stylix.colors;
       binde = , j, resizeactive, 0 10
       binde = , Return,submap,reset
       submap=reset
-
-
-
-      # Volume/media binds
-      bindel = , XF86AudioRaiseVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 2%+
-      bindel = , XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 2%-
-      bindl = , XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle
-      bindl = , XF86AudioMicMute, exec, wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle
-      # Skip player on long press and only skip 5s on normal press
-      #bindo = , XF86AudioNext, exec, playerctl next
-      bind = , XF86AudioNext, exec, playerctl position +5
-      # Requires playerctl
-      bindl = , XF86AudioPlay, exec, playerctl play-pause
-      bindl = , XF86AudioPrev, exec, playerctl previous
-      bindl = , XF86AudioNext, exec, playerctl next
-      # Brightness
-      bindel = ,XF86MonBrightnessDown, exec, sudo /run/current-system/sw/bin/light -U 10
-      bindel = ,XF86MonBrightnessUp, exec, sudo /run/current-system/sw/bin/light -A 10
-
-      # PrintScreen key bindings
-      # Screenshot a window
-      bind = $mod, PRINT, exec, hyprshot -m window
-      # Screenshot a monitor
-      bind = , PRINT, exec, hyprshot -m output
-      # Screenshot a region
-      bind = $mod SHIFT, PRINT, exec, hyprshot -m region
-
-      # Hyprlock when the lid is closed
-      bindl=,switch:on:Lid Switch, exec, hyprctl dispatch exec hyprlock && systemctl suspend
-
-
-
-      # to make kando work
-      windowrule = noblur, kando
-      windowrule = opaque, kando
-      windowrule = size 100% 100%, kando
-      windowrule = noborder, kando
-      windowrule = noanim, kando
-      windowrule = float, kando
-      windowrule = pin, kando
-
-
-      # To make KDE connect message reply window not be tiny
-      windowrulev2 = minsize 400 400, title:(Messages — KDE Connect Daemon)
-
-      # Calculator scratchpad
-      windowrulev2 = float,class:(kitty-calculator)
-      windowrulev2 = size 500 500,class:(kitty-calculator)
-      windowrulev2 = workspace special:calculator,class:(kitty-calculator)
-      bind = $mod, M, exec, hyprctl clients | grep "kitty-calculator" && hyprctl dispatch togglespecialworkspace calculator || kitty --class "kitty-calculator" qalc &
-
-      # Translate scratchpad
-      windowrulev2 = float,class:(kitty-translator)
-      windowrulev2 = size 800 800,class:(kitty-translator)
-      windowrulev2 = workspace special:translator,class:(kitty-translator)
-      bind = $mod, T, exec, hyprctl clients | grep "kitty-translator" && hyprctl dispatch togglespecialworkspace translator || kitty --class "kitty-translator" trans -theme random -I &
-
-      # LLM scratchpad
-      windowrulev2 = float,class:(kitty-ai)
-      windowrulev2 = size 800 800,class:(kitty-ai)
-      windowrulev2 = workspace special:ai,class:(kitty-ai)
-      #bind = $mod, A, exec, hyprctl clients | grep "kitty-ai" && hyprctl dispatch togglespecialworkspace ai || kitty --class "kitty-ai" tenere &
-      bind = $mod, A, exec, hyprctl clients | grep "kitty-ai" && hyprctl dispatch togglespecialworkspace ai || kitty --class "kitty-ai" tgpt -m &
-
-
-      bind = SUPER, grave, hyprexpo:expo, toggle # can be: toggle, off/disable or on/enable
-      #bind = SUPER, grave, overview:toggle
-
     '';
 
     # This adds specific packages as plugins, not configuring them
