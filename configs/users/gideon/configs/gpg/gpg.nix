159279f1535c161627e6102408b5e8914959a7f7
GideonWolfe <gideon@gideonwolfe.com>
2025-05-16 11:17:39 -0400

    making some changes

--- configs/users/gideon/configs/gpg/gpg.nix
+++ configs/users/gideon/configs/gpg/gpg.nix
@@ -5,5 +5,6 @@
     enable = true;
     # Always import my PGP public key into user config
     publicKeys = [{ source = ../gideon_pub.asc; }];
+    scdaemonSettings = {disable-ccid = true;}; # tell GPG to not use its own cardreader daemon and use PC/SC instead
   };
 }
