7555073ae722ab8d18593fe9560182531faa3391
GideonWolfe <gideon@gideonwolfe.com>
2025-09-08 01:40:58 -0400

    working install no network

--- configs/hosts/do-vps-test/configuration.nix
+++ configs/hosts/do-vps-test/configuration.nix
@@ -11,14 +11,14 @@
   #       #TEST: does this take from the key we selected during droplet creation?
   #       setSshKeys = true;
   #   };
-  services.cloud-init = {
-    enable = true;
-    settings.networking.enable = true;
-    #settings.ext4.enable = true;
-  };
 
-  boot.loader.grub.devices =
-    lib.mkForce [ config.disko.devices.disk.disk0.device ];
+  # maybe this makes SSH work?
+  networking.firewall.allowedTCPPorts = [ 22 80 443 ];
+
+  boot.loader.grub = {
+    efiSupport = true;
+    efiInstallAsRemovable = true;
+  };
 
   nix = {
     settings = {
@@ -38,24 +38,54 @@
     enable = true;
     settings = {
       PasswordAuthentication = false;
-      PermitRootLogin = "no";
+      #PermitRootLogin = "no";
     };
   };
 
-  disko.devices.disk = {
-    disk0 = {
-      device = "/dev/vda";
+  disko.devices = {
+    disk.disk1 = {
+      device = lib.mkDefault "/dev/vda";
       type = "disk";
       content = {
         type = "gpt";
         partitions = {
+          boot = {
+            name = "boot";
+            size = "1M";
+            type = "EF02";
+          };
+          esp = {
+            name = "ESP";
+            size = "500M";
+            type = "EF00";
+            content = {
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/boot";
+            };
+          };
           root = {
-            type = "8300"; # Linux filesystem partition type (GPT)
+            name = "root";
             size = "100%";
+            content = {
+              type = "lvm_pv";
+              vg = "pool";
+            };
+          };
+        };
+      };
+    };
+    lvm_vg = {
+      pool = {
+        type = "lvm_vg";
+        lvs = {
+          root = {
+            size = "100%FREE";
             content = {
               type = "filesystem";
               format = "ext4";
               mountpoint = "/";
+              mountOptions = [ "defaults" ];
             };
           };
         };
@@ -68,62 +98,62 @@
   # Digitalocean stuff
   # https://github.com/nix-community/nixos-anywhere-examples/blob/main/digitalocean.nix
 
-  # services.cloud-init = {
-  #   enable = true;
-  #   networking.enable = true;
-  #   settings = {
-  #     datasource_list = [ "ConfigDrive" "Digitalocean" ];
-  #     datasource.ConfigDrive = { };
-  #     datasource.Digitalocean = { };
-  #     # Based on https://github.com/canonical/cloud-init/blob/main/config/cloud.cfg.tmpl
-  #     cloud_init_modules = [
-  #       "seed_random"
-  #       "bootcmd"
-  #       "write_files"
-  #       "growpart"
-  #       "resizefs"
-  #       "set_hostname"
-  #       "update_hostname"
-  #       # Not support on NixOS
-  #       #"update_etc_hosts"
-  #       # throws error
-  #       #"users-groups"
-  #       # tries to edit /etc/ssh/sshd_config
-  #       #"ssh"
-  #       "set_password"
-  #     ];
-  #     cloud_config_modules = [
-  #       "ssh-import-id"
-  #       "keyboard"
-  #       # doesn't work with nixos
-  #       #"locale"
-  #       "runcmd"
-  #       "disable_ec2_metadata"
-  #     ];
-  #     ## The modules that run in the 'final' stage
-  #     cloud_final_modules = [
-  #       "write_files_deferred"
-  #       "puppet"
-  #       "chef"
-  #       "ansible"
-  #       "mcollective"
-  #       "salt_minion"
-  #       "reset_rmc"
-  #       # install dotty agent fails
-  #       #"scripts_vendor"
-  #       "scripts_per_once"
-  #       "scripts_per_boot"
-  #       # /var/lib/cloud/scripts/per-instance/machine_id.sh has broken shebang
-  #       #"scripts_per_instance"
-  #       "scripts_user"
-  #       "ssh_authkey_fingerprints"
-  #       "keys_to_console"
-  #       "install_hotplug"
-  #       "phone_home"
-  #       "final_message"
-  #     ];
-  #   };
-  # };
+  services.cloud-init = {
+    enable = true;
+    settings = {
+      networking.enable = true;
+      datasource_list = [ "ConfigDrive" "Digitalocean" ];
+      datasource.ConfigDrive = { };
+      datasource.Digitalocean = { };
+      # Based on https://github.com/canonical/cloud-init/blob/main/config/cloud.cfg.tmpl
+      cloud_init_modules = [
+        "seed_random"
+        "bootcmd"
+        "write_files"
+        "growpart"
+        "resizefs"
+        "set_hostname"
+        "update_hostname"
+        # Not support on NixOS
+        #"update_etc_hosts"
+        # throws error
+        #"users-groups"
+        # tries to edit /etc/ssh/sshd_config
+        #"ssh"
+        "set_password"
+      ];
+      cloud_config_modules = [
+        "ssh-import-id"
+        "keyboard"
+        # doesn't work with nixos
+        #"locale"
+        "runcmd"
+        "disable_ec2_metadata"
+      ];
+      ## The modules that run in the 'final' stage
+      cloud_final_modules = [
+        "write_files_deferred"
+        "puppet"
+        "chef"
+        "ansible"
+        "mcollective"
+        "salt_minion"
+        "reset_rmc"
+        # install dotty agent fails
+        #"scripts_vendor"
+        "scripts_per_once"
+        "scripts_per_boot"
+        # /var/lib/cloud/scripts/per-instance/machine_id.sh has broken shebang
+        #"scripts_per_instance"
+        "scripts_user"
+        "ssh_authkey_fingerprints"
+        "keys_to_console"
+        "install_hotplug"
+        "phone_home"
+        "final_message"
+      ];
+    };
+  };
   #
 
   networking.hostName = "wumpus"; # Define your hostname.
@@ -132,6 +162,7 @@
     home = "/home/gideon";
     isNormalUser = true;
     shell = pkgs.fish;
+    initialPassword = "pw123";
     extraGroups = [
       "wheel" # Enable ‘sudo’ for the user.
       "docker" # Let the user run docker commands
