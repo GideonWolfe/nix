4aaa2f525cea42c41615280187b528f80bb10f7a
GideonWolfe <gideon@gideonwolfe.com>
2025-09-23 22:48:42 -0400

    comments

--- configs/hosts/uconsole/configuration.nix
+++ configs/hosts/uconsole/configuration.nix
@@ -11,25 +11,24 @@ in {
     # Hardware configuration for Raspberry Pi
     "${modulesPath}/profiles/base.nix"
 
-
     # Rex Kernel # 
     ./kernels/rex/kernel.nix
     ./kernels/rex/boot.nix
     ./system.nix
     ./hardware.nix
     ./hardware-configuration.nix
-    
-     # Potato Kernel # 
-    ./kernels/potato/kernel.nix
-    ./kernels/rex/kernel.nix
-    ./boot.nix
-    ./system.nix
-    ./hardware.nix
-    ./dt-misc.nix
-    ./dt-display.nix
-    ./dt-pmu.nix
-    ./dt-overlays.nix
-    ./hardware-configuration.nix
+
+    # Potato Kernel # 
+    # ./kernels/potato/kernel.nix
+    # ./kernels/rex/kernel.nix
+    # ./boot.nix
+    # ./system.nix
+    # ./hardware.nix
+    # ./dt-misc.nix
+    # ./dt-display.nix
+    # ./dt-pmu.nix
+    # ./dt-overlays.nix
+    # ./hardware-configuration.nix
   ];
 
   # HACK: fix the bug with missing firmware
