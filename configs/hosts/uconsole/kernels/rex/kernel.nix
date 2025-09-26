d425bfc25a7514e58aa63a4094aa3c62b0b0aacb
GideonWolfe <gideon@gideonwolfe.com>
2025-09-25 23:25:43 -0400

    clean base

--- configs/hosts/uconsole/kernels/rex/kernel.nix
+++ configs/hosts/uconsole/kernels/rex/kernel.nix
@@ -1,11 +1,7 @@
 { config, lib, pkgs, ... }:
 
 {
-  # uConsole-specific kernel configuration with patches
-  # Based on the 6.1-potatomania kernel from nixos-uconsole but cleaned up
-
   boot = {
-    # Use a custom kernel package with uConsole-specific patches
     kernelPackages = pkgs.linuxPackagesFor (pkgs.linux_rpi4.override {
       argsOverride = {
         # Version for commit 99972b2fa5395542e7c24e4d894be5ede383055f
@@ -21,17 +17,32 @@
     });
 
     kernelPatches = [
-      # {
-      #   name = "clockworkpi-kernel";
-      #   patch = ./patches/clockworkpi-kernel.patch;
-      # }
-      # {
-      #   name = "clockworkpi-kernel";
-      #   patch = ./patches/clockworkpi-kernel-no-overlay.patch;
-      # }
       {
         name = "clockworkpi-kernel";
         patch = ./patches/clockworkpi-kernel-renamed-overlay.patch;
+      }
+      {
+        name = "uconsole-config";
+        patch = null;
+        extraStructuredConfig = with lib.kernel; {
+          # Enable the newly patched modules
+          DRM_PANEL_CWU50 = module;
+          BACKLIGHT_OCP8178 = module;
+
+          # Power management configuration from jhewitt.net/uconsole
+          REGMAP_I2C = yes;
+          INPUT_AXP20X_PEK = yes;
+          CHARGER_AXP20X = module;
+          BATTERY_AXP20X = module;
+          AXP20X_POWER = module;
+          MFD_AXP20X = yes;
+          MFD_AXP20X_I2C = yes;
+          REGULATOR_AXP20X = yes;
+          AXP20X_ADC = module;
+          TI_ADC081C = module;
+          CRYPTO_LIB_ARC4 = yes;
+          CRC_CCITT = yes;
+        };
       }
     ];
 
@@ -74,15 +85,17 @@
         [ "usbhid" "usb_storage" "vc4" "pcie_brcmstb" "reset-raspberrypi" ];
       # Load uConsole-specific modules at boot
       kernelModules = [
-        "ocp8178-bl" # Backlight driver
-        "panel-clockwork-cwu50" # Display panel driver
+        "ocp8178_bl" # Backlight driver
+        "panel-cwu50" # Display panel driver
+	      "axp20x_ac_power" # PMU driver
+	      "axp20x_battery" # Battery driver
       ];
     };
 
     # Disable Plymouth for cleaner boot process
     plymouth.enable = false;
   };
-
   # Firmware is handled by nixos-hardware raspberry-pi-4 module
   # which sets hardware.enableRedistributableFirmware = true
+  systemd.services."serial-getty@ttyS0".enable = false;
 }
