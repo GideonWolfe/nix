e2036def43dbf1ffa9ff941ae24ed22ab491daab
GideonWolfe <gideon@gideonwolfe.com>
2025-11-10 19:21:36 -0500

    whoops

--- configs/hosts/uconsole/clever/initial-setup.nix
+++ configs/hosts/uconsole/clever/initial-setup.nix
@@ -40,6 +40,14 @@ in {
       config_txt = pkgs.writeText "config.txt" ''
         initramfs initrd followkernel
         gpu_mem=16
+
+        # CM4 specific settings
+        # TODO copy to bootloader.nix so nixos-rebuild will work ON the system
+        [cm4]
+        arm_boost=1
+        max_framebuffers=2
+        dtoverlay=vc4-kms-v3d-pi4
+        dtoverlay=uconsole,cm4,hwi2c
       '';
     in ''
       cp -v ${fw}/{bootcode.bin,fixup.dat,start.elf,fixup4.dat,start4.elf,fixup4cd.dat,fixup_cd.dat,start4cd.elf,start_cd.elf,bcm2710*dtb,bcm2711*dtb,bcm2712*dtb} firmware/
---
+++ configs/hosts/uconsole/clever/reinstall-bootloader.sh
@@ -0,0 +1,14 @@
+#! @crossShell@
+
+set -e
+
+export PATH=/run/current-system/sw/bin/
+mkdir -pv /boot/firmware
+mount /boot/firmware
+
+cp -v @fw@/{bootcode.bin,fixup.dat,start.elf,fixup4.dat,start4.elf,fixup4cd.dat,fixup_cd.dat,start4cd.elf,start_cd.elf,bcm2710*dtb,bcm2711*dtb,bcm2712*dtb} /boot/firmware
+cat $1/kernel | gzip -9v > /boot/firmware/kernel8.img
+cp -vL $1/initrd /boot/firmware/initrd
+
+echo "init=$1/init $(cat $1/kernel-params)" > /boot/firmware/cmdline.txt
+cp -v @config_txt@ /boot/firmware/config.txt
