8a41b4782852fc7ebe635bdeef82dc74a3352f4d
GideonWolfe <gideon@gideonwolfe.com>
2025-12-27 21:13:40 -0500

    fix

--- configs/v3modules/home/apps/nixvim/plugins/git/diffview.nix
+++ configs/v3modules/home/apps/nixvim/plugins/git/diffview.nix
@@ -1,7 +1,9 @@
 {
   programs.nixvim.plugins.diffview = {
     enable = true;
-    enhanced_diff_hl = true;
-    use_icons = true;
+    settings = {
+      enhanced_diff_hl = true;
+      use_icons = true;
+    };
   };
 }
