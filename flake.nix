c09865f9a51126d9d45ed4723b07638f117c4c5b
GideonWolfe <gideon@gideonwolfe.com>
2025-12-27 20:35:16 -0500

    version bump

--- flake.nix
+++ flake.nix
@@ -3,12 +3,12 @@
 
   inputs = {
 
-    nixpkgs = { url = "github:NixOS/nixpkgs/nixos-25.05"; };
+    nixpkgs = { url = "github:NixOS/nixpkgs/nixos-25.11"; };
 
     nixpkgs-unstable = { url = "github:NixOS/nixpkgs/nixos-unstable"; };
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.05";
+      url = "github:nix-community/home-manager/release-25.11";
       inputs = { nixpkgs.follows = "nixpkgs"; };
     };
 
@@ -22,7 +22,7 @@
 
     # Theming engine
     stylix = {
-      url = "github:nix-community/stylix/release-25.05";
+      url = "github:nix-community/stylix/release-25.11";
       inputs = { nixpkgs.follows = "nixpkgs"; };
     };
 
@@ -34,7 +34,7 @@
 
     # Configure neovim with Nix!
     nixvim = {
-      url = "github:nix-community/nixvim/nixos-25.05";
+      url = "github:nix-community/nixvim/nixos-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
